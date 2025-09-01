6dcb7423d61510237e1f2bf6a2af38cf58afa077
talhaansari692 <talhaansari692@gmail.com>
2025-09-01 15:00:33 +0530

    Update ChannelPartnerAPI.jsx THIS IS URL CHANGE

--- src/apis/ChannelPartnerAPI.jsx
+++ src/apis/ChannelPartnerAPI.jsx
@@ -1,6 +1,7 @@
 // src/apis/channelPartnerApi.js
+const ENV = import.meta.env;
 export const postChannelPartner = async (data) => {
-  const response = await fetch("http://localhost:8080/api/v1/channel-partner", {
+  const response = await fetch(`${ENV.VITE_BASE_URL}/channel-partner`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
