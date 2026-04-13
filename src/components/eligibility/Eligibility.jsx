52a2591a85b9083fcd4cf30e8581d8188abc9f18
shnawaz844 <shnawaz844@users.noreply.github.com>
2026-04-13 12:39:42 +0530

    text changed was overlapping

--- src/components/eligibility/Eligibility.jsx
+++ src/components/eligibility/Eligibility.jsx
@@ -173,14 +173,14 @@ export default function Eligibility() {
                   {
                     src: "/Fingerprint.gif",
                     label: "Aadhar",
-                    alt: "Adhaar logo",
+                    alt: "Aadhar logo",
                   },
                   {
                     src: "/Financial.gif",
                     label: "Financial Statement",
-                    alt: "Adress logo",
+                    alt: "Financial Statement logo",
                   },
-                  { src: "/Wallet.gif", label: "PAN", alt: "Pan card logo" },
+                  { src: "/Wallet.gif", label: "PAN", alt: "PAN card logo" },
                 ].map((item, index) => (
                   <Grid item xs={8} sm={6} md={4} key={index}>
                     <Box
@@ -195,29 +195,22 @@ export default function Eligibility() {
                       <img
                         src={item.src}
                         style={{
-                          height: "100%",
+                          height: "140px",
                           width: "auto",
                           borderRadius: "20px",
+                          mixBlendMode: "multiply",
                         }}
-                        alt="Descriptive alternative text"
+                        alt={item.alt}
                       />
 
                       <Typography
                         sx={{
-                          position: "absolute",
-                          bottom: "5px",
-                          left: "50%",
-                          transform: "translateX(-50%)",
-                          color:
-                            index % 2
-                              ? theme.palette.secondary.main
-                              : theme.palette.secondary.main,
+                          color: theme.palette.secondary.main,
                           padding: {
-                            xs: "210px 15px",
+                            xs: "4px 10px",
                             sm: "4px 20px",
-                            md: "5px 30px",
+                            md: "4px 10px",
                           },
-                          Radius: "10px",
                           fontWeight: "bold",
                           textAlign: "center",
                           fontSize: {
@@ -229,7 +222,6 @@ export default function Eligibility() {
                       >
                         {item.label}
                       </Typography>
-                      <Typography>{item.alt}</Typography>
                     </Box>
                   </Grid>
                 ))}
