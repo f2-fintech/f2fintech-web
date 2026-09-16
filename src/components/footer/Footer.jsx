b6e1cf2b4fc59e2aea32ebb4a6434d015ae500fb
shnawaz844 <shnawaz844@users.noreply.github.com>
2026-09-16 15:38:28 +0530

    new change1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -448,7 +448,7 @@ const Footer = () => {
                 fontFamily: "Poppins",
               }}
             >
-              © 2026 F2 Fintech Private Limited. All Rights Reserved.
+              © 2026 All Rights Reserved by F2 Fintech Private Limited
             </Typography>
           </Box>
         </Box>
